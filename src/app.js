dcf6f9758248315b2b6a91b0a1ba40c8653fade2
Agyeibea Antwi-Baadu <agyeibeaantwibaadu@gmail.com>
2026-03-06 21:12:15 +0000

    Add auth and task controllers, clean up project structure

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
+import dotenv from "dotenv";
 import express from "express";
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 
+dotenv.config();   // <-- ADD THIS
+
 const app = express();
+
+// connect database
+connectDB();
 
 app.use(express.json());
 
